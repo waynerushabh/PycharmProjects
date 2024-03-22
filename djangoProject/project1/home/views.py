a7b7c517623aeb474525bc43708f8210daa3fdf1
waynerushabh <111569093+waynerushabh@users.noreply.github.com>
2024-03-22 20:54:05 +0530

    More from MacOS
    - Moved Pytestbdd with good files
    - Deleted a basic Django project

--- djangoProject/project1/home/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-
-
-def welcome(request):
-    return HttpResponse("<i><b><h1>Welcome Home!</h1></b></i>")
